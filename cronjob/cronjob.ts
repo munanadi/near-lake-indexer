cef7b7d0839dc359a5c1e96eec251bffee49a901
pisomanik <ajibaa@gmail.com>
2022-08-02 10:54:53 +0530

    [WIP] add local data, case for all dexes
    TODO: actual calculation, double check token pool order

--- cronjob/cronjob.ts
+++ cronjob/cronjob.ts
@@ -3,6 +3,7 @@ import {
   FormattedPool,
   InMemoryProvider,
   RefPool,
+  StablePool,
   TonicMarket,
 } from '@arbitoor/arbitoor-core';
 import { Market as SpinMarket } from '@spinfi/core';
@@ -11,64 +12,98 @@ import axios from 'axios';
 import { MainnetRpc } from 'near-workspaces';
 import Big from 'big.js';
 
+// import data from local file instead of fetching always
+// @ts-ignore
+import localData from './data/data';
+
+// Used to pull data to a local file
+// import { createLogger, transports } from 'winston';
+
+// const logger = createLogger({
+//   transports: [
+//     new transports.File({
+//       filename: './data/data.json',
+//       level: 'info',
+//     }),
+//   ],
+// });
+
 const pool = new Pool({
   connectionString: 'postgres://postgres:root@localhost:5432/postgres',
 });
 
-// To fetch token lists
-let provider: InMemoryProvider | null;
-let tokenMap: Map<string, TokenInfo>;
-let tonicMarketMap: Map<string, TonicMarket>;
-let spinMarketMap: Map<number, SpinMarket>;
-
 (async () => {
-  /**
-   * Setup - fetch tonic markets
-   */
+  // If needed to pull data
+  // const tokens = await new TokenListProvider().resolve();
+  // const tokenList = tokens.filterByNearEnv('mainnet').getList();
+  const tokenList = localData.tokensMap as Array<TokenInfo>;
 
-  console.log('Setting up');
-  const tokens = await new TokenListProvider().resolve();
-  const tokenList = tokens.filterByNearEnv('mainnet').getList();
-  // console.log(tokenList);
-  tokenMap = tokenList.reduce((map, item) => {
+  // // console.log(tokenList);
+  const tokensMap = tokenList.reduce((map, item) => {
     map.set(item.address, item);
     return map;
   }, new Map<string, TokenInfo>());
 
-  provider = new InMemoryProvider(MainnetRpc, tokenMap);
-
-  await provider.fetchPools();
+  // let provider = new InMemoryProvider(MainnetRpc, tokensMap);
+  // await provider.fetchPools();
 
   // const tonicMarkets = provider.getTonicMarkets();
   // const spinMarkets = provider.getSpinMarkets();
-  const refPools = provider.getRefPools();
-  const refStablePools = provider.getRefStablePools();
+  // const refPools = provider.getRefPools();
+  // const refStablePools = provider.getRefStablePools();
+  // const jumboPools = provider.getJumboPools();
+  // const jumboStablePools = provider.getJumboStablePools();
 
-  const refMarketMap = new Map<string, string[]>();
+  // Reading data from local file
+  const tonicMarkets = localData.tonicMarkets as Array<TonicMarket>;
+  const spinMarkets = localData.spinMarkets as Array<SpinMarket>;
+  const refPools = localData.refPools as Array<FormattedPool>;
+  const refStablePools = localData.refStablePools as Array<StablePool>;
+  const jumboPools = localData.jumboPools as Array<FormattedPool>;
+  const jumboStablePools = localData.jumboStablePools as Array<StablePool>;
+
+  // logger.info(tonicMarkets);
+
+  const refMarketTokenMap = new Map<string, string[]>();
 
   for (const pool of refPools) {
-    refMarketMap.set(pool.id.toString(), pool.token_account_ids);
+    refMarketTokenMap.set(pool.id.toString(), pool.token_account_ids);
   }
   for (const pool of refStablePools) {
-    refMarketMap.set(pool.id.toString(), pool.token_account_ids);
+    refMarketTokenMap.set(pool.id.toString(), pool.token_account_ids);
   }
 
-  // console.table(refMarketMap);
+  const jumboMarketTokenMap = new Map<string, string[]>();
 
-  // tonicMarketMap = tonicMarkets.reduce((map, item) => {
-  //   map.set(item.id, item);
-  //   return map;
-  // }, new Map<string, TonicMarket>());
+  for (const pool of jumboPools) {
+    jumboMarketTokenMap.set(pool.id.toString(), pool.token_account_ids);
+  }
+  for (const pool of jumboStablePools) {
+    jumboMarketTokenMap.set(pool.id.toString(), pool.token_account_ids);
+  }
 
-  // spinMarketMap = spinMarkets.reduce((map, item) => {
-  //   map.set(item.id, item);
-  //   return map;
-  // }, new Map<number, SpinMarket>());
+  const tonicMarketTokenMap = tonicMarkets.reduce((map, item) => {
+    const baseToken =
+      (item.base_token.token_type['type'] === 'ft'
+        ? item.base_token.token_type['account_id']
+        : item.base_token.token_type['type']) ?? '';
+    const quoteToken =
+      (item.quote_token.token_type['type'] === 'ft'
+        ? item.quote_token.token_type['account_id']
+        : item.quote_token.token_type['type']) ?? '';
+    map.set(item.id, [baseToken, quoteToken]);
+    return map;
+  }, new Map<string, string[]>());
+
+  const spinMarketTokenMap = spinMarkets.reduce((map, item) => {
+    const baseToken = item.base.address;
+    const quoteToken = item.quote.address;
+    map.set(item.id, [baseToken, quoteToken]);
+    return map;
+  }, new Map<number, string[]>());
 
   // // fetch and store prices
   // await fetchTokenPrices();
-
-  // await startStream(lakeConfig, handleStreamerMessage);
 
   const dailyVolume: {
     [date: number]: string;
@@ -102,14 +137,29 @@ let spinMarketMap: Map<number, SpinMarket>;
 
       switch (row.dex) {
         case 'v2.ref-finance.near': {
-          tokenIn = refMarketMap.get(row.pool_id.toString())?.[0] ?? '';
-          tokenOut = refMarketMap.get(row.pool_id.toString())?.[1] ?? '';
+          tokenIn = refMarketTokenMap.get(row.pool_id.toString())?.[0] ?? '';
+          tokenOut = refMarketTokenMap.get(row.pool_id.toString())?.[1] ?? '';
+          break;
+        }
+        case 'v1.jumbo_exchange.near': {
+          tokenIn = jumboMarketTokenMap.get(row.pool_id.toString())?.[0] ?? '';
+          tokenOut = jumboMarketTokenMap.get(row.pool_id.toString())?.[1] ?? '';
+          break;
+        }
+        case 'v1.orderbook.near': {
+          tokenIn = tonicMarketTokenMap.get(row.pool_id.toString())?.[0] ?? '';
+          tokenOut = tonicMarketTokenMap.get(row.pool_id.toString())?.[1] ?? '';
+          break;
+        }
+        case 'spot.spin-fi.near': {
+          tokenIn = spinMarketTokenMap.get(row.pool_id.toString())?.[0] ?? '';
+          tokenOut = spinMarketTokenMap.get(row.pool_id.toString())?.[1] ?? '';
           break;
         }
       }
 
-      const token0Decimals = tokenMap.get(tokenIn)?.decimals ?? 1;
-      const token1Decimals = tokenMap.get(tokenOut)?.decimals ?? 1;
+      const token0Decimals = tokensMap.get(tokenIn)?.decimals ?? 1;
+      const token1Decimals = tokensMap.get(tokenOut)?.decimals ?? 1;
 
       console.log(
         row.amount_in,
@@ -132,10 +182,6 @@ let spinMarketMap: Map<number, SpinMarket>;
 // Fetch token prices every 30 mins
 let tokenPrices = new Map<string, number>();
 let cachedTokenPrices = new Map<string, number>();
-
-setInterval(async () => {
-  // await fetchTokenPrices();
-}, 1000 * 60 * 30);
 
 async function fetchTokenPrices() {
   console.log('Fetching token prices');
---
+++ cronjob/data/README.md
@@ -0,0 +1,1 @@
+Pull data to a local file here since fethcing these on every run was taking a while and sometimes would fail with a gateway timeout error
